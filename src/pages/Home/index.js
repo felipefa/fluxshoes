cb92c806a0c8137969a19521bd9dee27e482bfc5
Felipe Araujo <devaraujofelipe@gmail.com>
2019-10-12 11:23:57 -0300

    Style home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-adidas-predator-19-3-in/06/COL-4165-006/COL-4165-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Nome Tênis</strong>
+        <span>R$199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-adidas-predator-19-3-in/06/COL-4165-006/COL-4165-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Nome Tênis</strong>
+        <span>R$199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-adidas-predator-19-3-in/06/COL-4165-006/COL-4165-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Nome Tênis</strong>
+        <span>R$199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-adidas-predator-19-3-in/06/COL-4165-006/COL-4165-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Nome Tênis</strong>
+        <span>R$199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-adidas-predator-19-3-in/06/COL-4165-006/COL-4165-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Nome Tênis</strong>
+        <span>R$199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-adidas-predator-19-3-in/06/COL-4165-006/COL-4165-006_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Nome Tênis</strong>
+        <span>R$199,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
